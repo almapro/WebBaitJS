6689ad00b7da3c28dd0a02be51740a155702eaca
AlMA PRO <alma.pro.leader@gmail.com>
2022-03-16 13:18:10 +0200

    feat(client): uses home view title

--- packages/client/src/views/home.view.tsx
+++ packages/client/src/views/home.view.tsx
@@ -1,10 +1,14 @@
 import { AutoAwesome as AutoAwesomeIcon } from "@mui/icons-material";
 import { Button, Grid } from "@mui/material";
 import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { useTitle } from "react-use";
 
 export type HomeViewProps = {};
 
 export const HomeView: FC<HomeViewProps> = () => {
+  const { t } = useTranslation();
+  useTitle(`WebBait - ${t('titles.home')}`);
   return (
     <Grid container spacing={2} p={2}>
       <Grid item xs={12}>
