b7563aafb769ac33c3ff4ce1d2092fd06503f537
AlMA PRO <alma.pro.leader@gmail.com>
2022-05-30 02:32:57 +0200

    feat(agent): adds images assets

---
+++ packages/agent/src/images/index.ts
@@ -0,0 +1,13 @@
+import MessengerIconSvg from './messenger.svg';
+import CPanelIconSvg from './cpanel.svg';
+import CPanelUsernameIcon from './cpanel-username.png';
+import CPanelPasswordIcon from './cpanel-password.png';
+import CPanelSmallIconSvg from './cp-small.svg';
+
+export {
+  MessengerIconSvg,
+  CPanelIconSvg,
+  CPanelUsernameIcon,
+  CPanelPasswordIcon,
+  CPanelSmallIconSvg,
+}
